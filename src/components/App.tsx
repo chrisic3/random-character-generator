9db57285eb0d5de2ff43a3950f17ddeabfdda696
chrisic3 <chris_ic3@hotmail.com>
2026-09-08 19:55:39 +0000

    chore: fix import file paths

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,23 @@
+import type { Category, Results } from "../types/Types";
+import { useState } from "react";
+
+const categories: Category[] = [
+  {
+    id: 1,
+    name: "Hair Color",
+    items: ["Black", "Brown", "Blonde"],
+  },
+  {
+    id: 2,
+    name: "Hair Type",
+    items: ["Bald", "Straight", "Wavy", "Curly", "Short", "Long"],
+  },
+];
+
 function App() {
+  for (let i = 0; i < categories.length; i++) {
+    console.log(categories[i].items);
+  }
   return <div></div>;
 }
 
