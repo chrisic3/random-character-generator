73b57ce64fc854563a14d4325bad209bdc227156
chrisic3 <chris_ic3@hotmail.com>
2026-09-16 21:38:44 -0500

    test: add more slice geometry tests

--- src/lib/sliceGeometry.test.ts
+++ src/lib/sliceGeometry.test.ts
@@ -4,12 +4,44 @@ import { getSliceData } from "./sliceGeometry";
 describe("getSliceData", () => {
   it("4 items return 4 slices", () => {
     // arrange
+    const center = 250;
+    const radius = 200;
     const items = ["A", "B", "C", "D"];
 
     // act
-    const result = getSliceData(0, 0, items);
+    const result = getSliceData(center, radius, items);
 
     // assert
     expect(result).toHaveLength(4);
+  });
+
+  it("sets largeArcFlag correctly for angles UNDER or EQUAL TO 180 degrees", () => {
+    // arrange
+    const center = 250;
+    const radius = 200;
+    const items = ["A", "B", "C", "D"];
+
+    // act
+    const result = getSliceData(center, radius, items);
+
+    // assert
+    result.forEach((slice) => {
+      expect(slice.largeArcFlag <= 180 ? 0 : 1).toBe(0);
+    });
+  });
+
+  it("sets largeArcFlag correctly for angles OVER 180 degrees", () => {
+    // arrange
+    const center = 250;
+    const radius = 200;
+    const items = ["A"];
+
+    // act
+    const result = getSliceData(center, radius, items);
+
+    // assert
+    result.forEach((slice) => {
+      expect(slice.largeArcFlag > 180 ? 1 : 0).toBe(1);
+    });
   });
 });
