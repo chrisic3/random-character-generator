c83cd6f2f981687144c725cbd39e04ced908f07a
chrisic3 <chris_ic3@hotmail.com>
2026-09-23 20:55:01 -0500

    feat: add remaining categories

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,20 +5,60 @@ import Wheel from "./Wheel";
 const categories: Category[] = [
   {
     id: 1,
+    name: "Hair Type",
+    items: ["Straight", "Wavy", "Curly", "Short", "Long", "None"],
+  },
+  {
+    id: 2,
     name: "Hair Color",
     items: ["Black", "Brown", "Blonde", "Red"],
   },
   {
-    id: 2,
-    name: "Hair Type",
-    items: ["Bald", "Straight", "Wavy", "Curly", "Short", "Long"],
+    id: 3,
+    name: "Eye Color",
+    items: ["None"],
+  },
+  {
+    id: 4,
+    name: "Horns",
+    items: ["None"],
+  },
+  {
+    id: 5,
+    name: "Wings",
+    items: ["None"],
+  },
+  {
+    id: 6,
+    name: "Tail",
+    items: ["None"],
+  },
+  {
+    id: 7,
+    name: "Accessory",
+    items: ["None"],
+  },
+  {
+    id: 8,
+    name: "Familiar",
+    items: ["None"],
+  },
+  {
+    id: 9,
+    name: "Class",
+    items: [],
+  },
+  {
+    id: 10,
+    name: "Archetype",
+    items: [],
   },
 ];
 
 function App() {
   return (
     <>
-      <Wheel items={categories[1].items} />
+      <Wheel items={categories[0].items} />
     </>
   );
 }
